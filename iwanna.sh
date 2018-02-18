8113ef86d6d80249b9b246c647fef63915dcdeac
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2018-02-18 15:09:19 +0100

    Shellcheck CI

--- iwanna.sh
+++ iwanna.sh
@@ -4,18 +4,19 @@ IFS=$'\n\t'
 
 if [ $# -lt 1 ]; then
   echo "$(tput bold)Available installers$(tput sgr0)"
-  ls installers/
+  find installers/ -type f -exec basename --suffix=.sh -- {} \; | sort
   exit
 fi
 
 package=$1
-if [ ! -f installers/"$package" ]; then
+if [ ! -f installers/"$package.sh" ]; then
   echo "$(tput setaf 1)No installer for $package$(tput sgr0)" >&2
   exit 1
 fi
 
-source installers/"$package"
+source installers/"$package.sh"
 
+# shellcheck disable=SC2034
 if declare -F check > /dev/null 2>&1; then
   check
 elif dpkg -s "$package" > /dev/null 2>&1; then
@@ -27,6 +28,7 @@ if declare -F pre-install > /dev/null 2>&1; then
   pre-install
 fi
 
+# shellcheck disable=SC2034
 if declare -F install > /dev/null 2>&1; then
   install
 else
