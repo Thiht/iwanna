0b302e58d6256c989a99a7164b41f52528ce01fb
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2018-02-18 16:44:13 +0100

    Fix CI

--- iwanna.sh
+++ iwanna.sh
@@ -2,19 +2,20 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [ $# -lt 1 ]; then
+if (( $# < 1 )); then
   echo "$(tput bold)Available installers$(tput sgr0)"
   find installers/ -type f -exec basename --suffix=.sh -- {} \; | sort
   exit
 fi
 
 package=$1
-if [ ! -f installers/"$package.sh" ]; then
+if [ ! -f "installers/$package.sh" ]; then
   echo "$(tput setaf 1)No installer for $package$(tput sgr0)" >&2
   exit 1
 fi
 
-source installers/"$package.sh"
+# shellcheck disable=SC1090
+source "installers/$package.sh"
 
 # shellcheck disable=SC2034
 if declare -F check > /dev/null 2>&1; then
