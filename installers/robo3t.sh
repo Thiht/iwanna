eb7ed9830a642d82106acbefef5c9dcadf3e7e96
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2018-03-25 19:34:00 +0200

    Improve curl call

---
+++ installers/robo3t.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+VERSION=1.1.1
+HASH=c93c6b0
+OS=linux
+ARCH=x86_64
+
+_troubleshooting=$(cat <<END
+$(tput bold)Troubleshooting$(tput sgr0)
+Issue
+    Error message: This application failed to start because it could not find or load the Qt platform plugin "xcb" in "".
+See
+    https://github.com/Studio3T/robomongo/issues/1385
+Fix
+    rm /opt/robo3t-$VERSION-$OS-$ARCH-$HASH/lib/libstdc++*
+END
+)
+
+install() {
+  # Download archive
+  curl -#Lo /tmp/robo3t.tar.gz "https://download.robomongo.org/$VERSION/linux/robo3t-$VERSION-$OS-$ARCH-$HASH.tar.gz"
+
+  # Install
+  sudo tar -C /opt -xzf /tmp/robo3t.tar.gz
+  sudo ln -s "/opt/robo3t-$VERSION-$OS-$ARCH-$HASH/bin/robo3t" /usr/bin/robo3t
+
+  # Custom
+  echo "$_troubleshooting"
+
+  # Cleanup
+  rm /tmp/robo3t.tar.gz
+}
