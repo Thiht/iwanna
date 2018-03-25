c7e125a3680be10406dbc3ef4c0fb836c6b6e3af
Thibaut Rousseau <thibaut.rousseau@protonmail.com>
2018-03-25 19:34:55 +0200

    Add pencil

---
+++ installers/pencil.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# http://pencil.evolus.vn/Downloads.html
+
+set -euo pipefail
+IFS=$'\n\t'
+
+VERSION=3.0.4
+ARCH=amd64
+
+install() {
+  # Download archive
+  curl -#Lo /tmp/pencil.deb "http://pencil.evolus.vn/dl/V$VERSION/Pencil_${VERSION}_$ARCH.deb"
+
+  # Install
+  sudo dpkg -i /tmp/pencil.deb
+
+  # Cleanup
+  rm /tmp/pencil.deb
+}
